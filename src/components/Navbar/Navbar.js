8bdb1ea298f863f7409c58680f9191e3d7a87236
Juan Cahuana <jcahuanam@gmail.com>
2020-01-23 01:53:11 -0500

    Refactoriza métodos de menu de navegación y agrega ícono de cerrar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,45 +1,37 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import SignedInLinks from './SignedInLinks';
-import SignedOutLinks from './SignedOutLinks';
+import SignedInLinks from './SignedInlinks/SignedInLinks';
+import SignedOutLinks from './SignedOutLinks/SignedOutLinks';
+import { MenuIcon, CloseIcon } from '../Icons/Icons';
 
-function Navbar(props) {
-  const { auth } = props;
-  const [isOpen, setIsOpen] = useState(false);
+function Navbar({ auth }) {
+  const [openMenu, setOpenMenu] = useState(false);
 
   const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  const icon = openMenu ? <CloseIcon /> : <MenuIcon />;
+  const menuStyles = openMenu ? 'm-0' : '-m-64';
+  const overlay = (
+    <div className="navbar__overlay bg-black fixed w-full h-full left-0 top-0 z-5 opacity-75" />
+  );
 
   return (
     <nav className="navbar border-b border-gray-200">
       <div className="navbar__content max-w-5xl mx-auto">
         <div className="flex content-center bg-white relative z-10 md:hidden">
           <button
-            onClick={() => {
-              setIsOpen(!isOpen);
-            }}
+            onClick={() => setOpenMenu(!openMenu)}
             type="button"
             className="block p-3 hover:text-red-600"
           >
-            <svg
-              className="w-5 h-5 fill-current text-gray-600 hover:text-red-600"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
+            {icon}
           </button>
         </div>
-
         <div
-          className={`fixed bg-white h-full w-64 ${
-            isOpen ? 'm-0' : '-m-64'
-          } z-10 md:static md:w-auto md:flex md:m-auto`}
+          className={`navbar__buttons fixed bg-white h-full w-64 z-10 ${menuStyles} md:static md:w-auto md:flex md:m-auto`}
         >
           {links}
         </div>
-        {isOpen ? (
-          <div className="bg-black fixed w-full h-full left-0 top-0 z-5 opacity-75" />
-        ) : null}
+        {openMenu ? overlay : null}
       </div>
     </nav>
   );
