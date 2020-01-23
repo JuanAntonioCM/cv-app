36e2004f8e17400c1ad829abb5b75be36e5ce342
Juan Cahuana <jcahuanam@gmail.com>
2020-01-23 01:47:55 -0500

    Agrega método para eliminar una habilidad

--- src/store/actions/skillActions.js
+++ src/store/actions/skillActions.js
@@ -1,6 +1,6 @@
 // Fuente de información para el Store, envía datos desde la aplicación al store
 // El dispatcher cumple la función de mediador entre las acciones y el store
-const createSkill = skill => {
+const createSkillAction = skill => {
   // Ejemplo síncrono
   /* return {
     type: 'ADD_SKILL',
@@ -19,12 +19,28 @@ const createSkill = skill => {
         return { skill, firestoreResponse };
       })
       .catch(error => {
-        console.log('error', error);
-
         dispatch({ type: 'CREATE_SKILL_ERROR', error });
         return error;
       });
   };
 };
 
-export { createSkill };
+const deleteSkillAction = documentID => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection('skills')
+      .doc(documentID)
+      .delete()
+      .then(firestoreResponse => {
+        dispatch({ type: 'DELETE_SKILL', documentID });
+        return { documentID, firestoreResponse };
+      })
+      .catch(error => {
+        dispatch({ type: 'DELETE_SKILL_ERROR', error });
+        return error;
+      });
+  };
+};
+
+export { createSkillAction, deleteSkillAction };
