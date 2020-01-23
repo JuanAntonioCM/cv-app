3ca340b6d6d71325eefdd674483ab5aafe798903
Juan Cahuana <jcahuanam@gmail.com>
2020-01-23 01:54:29 -0500

    Renombra componente Resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
-import PersonalData from './PersonalData';
-import Skills from './Skills';
+import PersonalData from './PersonalData/PersonalData';
+import Skills from './Skills/Skills';
 
 function Resume({ skills }) {
   return (
     <main className="main flex flex-wrap max-w-5xl mx-auto">
-      <div className="w-full md:w-1/3 p-3 bg-blue-800 pt-8">
+      <div className="w-full md:w-1/3 px-5 py-8 bg-blue-800 ">
         <PersonalData />
       </div>
-      <div className="w-full md:w-2/3 p-3 pt-8">
+      <div className="w-full md:w-2/3 px-5 py-8">
         <Skills skills={skills} />
       </div>
     </main>
