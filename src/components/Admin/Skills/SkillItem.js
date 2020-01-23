5530ec0b2251e6be7bc6dbd7c62cc99ce88de33e
Juan Cahuana <jcahuanam@gmail.com>
2020-01-23 02:08:19 -0500

    Agrega eliminación de habilidad

--- src/components/Admin/Skills/SkillItem.js
+++ src/components/Admin/Skills/SkillItem.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import { IconButton, TableRow, TableCell } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { deleteSkillAction } from '../../../store/actions/skillActions';
 
-function SkillItem({ skill }) {
+function SkillItem({ skill, deleteSkill }) {
   return (
     <TableRow>
       <TableCell>{skill.name}</TableCell>
-      <TableCell>{skill.level}</TableCell>
       <TableCell>
-        <IconButton edge="end" aria-label="delete" key={skill.id}>
+        <span style={{textTransform: 'capitalize'}}>{skill.level}</span>
+      </TableCell>
+      <TableCell>
+        <IconButton
+          edge="end"
+          aria-label="delete"
+          key={skill.id}
+          onClick={() => deleteSkill(skill.id)}
+        >
           <DeleteIcon />
         </IconButton>
       </TableCell>
@@ -16,4 +26,14 @@ function SkillItem({ skill }) {
   );
 }
 
-export default SkillItem;
+// export default SkillItem;
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteSkill(documentID) {
+      dispatch(deleteSkillAction(documentID));
+    }
+  };
+};
+
+// First param is mapStateToProps
+export default connect(null, mapDispatchToProps)(SkillItem);
